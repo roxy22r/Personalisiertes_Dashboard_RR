cd2eb287db685c9f2332bf8b79287ba0c3ef324c
roxy22r <raksana280@gmail.com>
2022-06-03 23:08:19 +0200

    RAK added Funcitonal navigation

--- src/main/java/com/example/personalisiertes_dashboard_rr/CalenderController.java
+++ src/main/java/com/example/personalisiertes_dashboard_rr/CalenderController.java
@@ -3,29 +3,39 @@ package com.example.personalisiertes_dashboard_rr;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 
+
 public class CalenderController {
     @FXML
-    private Label welcomeText;
-
+    private  Stage stage;
+    private  Scene scene;
+    private  Parent root;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onClickGetAJokeView(ActionEvent event) throws IOException {
+         root = FXMLLoader.load(getClass().getResource("/com/example/personalisiertes_dashboard_rr/getAJoke-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     @FXML
-    protected void onClickGetAJoke(ActionEvent event) throws IOException {
-
-
-
-
+    public void onClickGetStockView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/example/personalisiertes_dashboard_rr/stock-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
--- src/main/java/com/example/personalisiertes_dashboard_rr/GetAJokeController.java
+++ src/main/java/com/example/personalisiertes_dashboard_rr/GetAJokeController.java
@@ -1,4 +1,36 @@
 package com.example.personalisiertes_dashboard_rr;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class GetAJokeController {
+    @FXML
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void onClickGetCalenderView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/example/personalisiertes_dashboard_rr/calender-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    public void onClickGetStockView(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/com/example/personalisiertes_dashboard_rr/stock-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 }
