251a78f1a6b3384d27151aaff4bee8cadcec30ce
roxy22r <raksana280@gmail.com>
2022-06-03 22:33:10 +0200

    RAK updated Calender view

--- src/main/java/com/example/personalisiertes_dashboard_rr/CalenderController.java
+++ src/main/java/com/example/personalisiertes_dashboard_rr/CalenderController.java
@@ -1,14 +1,31 @@
 package com.example.personalisiertes_dashboard_rr;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
+
 
 public class CalenderController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+    @FXML
+    protected void onClickGetAJoke(ActionEvent event) throws IOException {
+
+
+
+
+    }
+
 }
