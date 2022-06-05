27912b784e28dcc39e47f230c67ced85887805ad
roxy22r <raksana280@gmail.com>
2022-06-05 15:49:58 +0200

    RAK update

--- src/main/java/com/example/personalisiertes_dashboard_rr/GetAJokeController.java
+++ src/main/java/com/example/personalisiertes_dashboard_rr/GetAJokeController.java
@@ -6,9 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class GetAJokeController {
     @FXML
@@ -24,13 +29,34 @@ public class GetAJokeController {
         stage.setScene(scene);
         stage.show();
     }
+
     @FXML
     public void onClickGetStockView(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/com/example/personalisiertes_dashboard_rr/stock-view.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
 
+    @FXML
+    public void onClickShowAJoke(ActionEvent event) throws IOException {
+        Text text = new Text();
+        text.setStyle("-fx-font: 24 arial;");
+        Alert dialog = new Alert(Alert.AlertType.NONE);
+        dialog.setContentText("A Joke");
+        dialog.setResizable(false);
+        dialog.initStyle(StageStyle.UTILITY);
+        dialog.getDialogPane().setMinSize(900, 330);
+        dialog.getDialogPane().setPrefSize(900, 330);
+        dialog.getDialogPane().setStyle("-fx-background-color: #97d1a4;");
+        ButtonType OK = new ButtonType("OK");
+        dialog.getButtonTypes().setAll(OK);
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.get() == OK) {
+            System.out.println("Approve Button is clicked");
+        }
+
+
+    }
 }
