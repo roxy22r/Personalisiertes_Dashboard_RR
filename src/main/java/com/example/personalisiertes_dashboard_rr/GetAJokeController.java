895a7cc035d4cf7015a233aee948de7f89d143e7
roxy22r <raksana280@gmail.com>
2022-06-17 19:42:29 +0200

    RAK added 20 Jokes

--- src/main/java/com/example/personalisiertes_dashboard_rr/GetAJokeController.java
+++ src/main/java/com/example/personalisiertes_dashboard_rr/GetAJokeController.java
@@ -31,7 +31,25 @@ public class GetAJokeController {
     private List<Joke> jokes = new ArrayList<>(Arrays.asList(
             new Joke("A woman gets on a bus with her baby. The bus driver says: 'Ugh, that’s the ugliest baby I’ve ever seen!' The woman walks to the rear of the bus and sits down, fuming. She says to a man next to her: “The driver just insulted me!' The man says: 'You go up there and tell him off. Go on, I’ll hold your monkey for you.'"),
             new Joke("I said to the Gym instructor 'Can you teach me to do the splits?'He said, “How flexible are you?' I said, 'I can’t make Tuesdays.'"),
-            new Joke("Police arrested two kids yesterday, one was drinking battery acid, the other was eating fireworks. They charged one – and let the other one off.")
+            new Joke("Police arrested two kids yesterday, one was drinking battery acid, the other was eating fireworks. They charged one – and let the other one off."),
+            new Joke("What’s the smartest insect? A spelling bee!"),
+            new Joke("How does the ocean say hi? It waves!"),
+            new Joke("How does NASA organize a party?\nThey planet."),
+            new Joke("There are two muffins baking in the oven. One muffin says to the other, “Phew, is it getting hot in here or is it just me?” The other muffin says, “AAAAHHH!! A TALKING MUFFIN!"),
+            new Joke("My memory has gotten so bad it has actually caused me to lose my job. I’m still employed. I just can’t remember where."),
+            new Joke("What does the zero say to the eight? A: Nice belt!"),
+            new Joke("A patient came to the hospital with a burned right hand. As the doctor took down his medical history, he asked the injured man, “Do you smoke?” “Yeah, a pack..."),
+            new Joke("What was Beethoven’s favorite fruit? BA-NA-NA-NAAAAAA."),
+            new Joke("In Fort Worth, Texas, I was hauled before the judge for driving with expired license plates. The judge listened attentively while I gave him a long, plausible explanation.\n" +
+                    "Then he said with great courtesy, \"My dear sir, we are not blaming you—we're just fining you.\""),
+            new Joke("I got excited when my son joined the cross-country team. But then I learned they don't cross the country and are back home in a few hours."),
+            new Joke("Q: If they hold a pie eating contest this holiday season, which song will they sing? \nA: Oh, Come all ye facefuls."),
+            new Joke("Q: What do you call a snowman that can walk? A: Snow-mobile"),
+            new Joke("Q: Why is Cinderella so bad at soccer? \nA: Because she always runs away from the ball!"),
+            new Joke("Q: Why is Peter Pan flying all the time?\n A: He Neverlands!"),
+            new Joke("Q: Why aren't dogs good dancers? A. Because they have two left feet!"),
+            new Joke("Who’s there?\nTheodore.\nTheodore who?\nTheodore wasn’t opened so I knocked."),
+            new Joke("What kind of dog does a magician have?\n best jokes for kids\n A Labracadabrador!")
     ));
 
 
@@ -69,6 +87,7 @@ public class GetAJokeController {
         dialog.getDialogPane().setPrefSize(500, 230);
         dialog.getDialogPane().setStyle("-fx-background-color: #97d1a4;");
         dialog.getDialogPane().getScene().setFill(Color.WHITE);
+
         ButtonType OK = new ButtonType("OK");
         dialog.getButtonTypes().setAll(OK);
         Optional<ButtonType> result = dialog.showAndWait();
